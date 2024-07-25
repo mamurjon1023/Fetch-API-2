a13cc67b921955148a007b9a2b87cbae075b20fa
mamurjon1148 <mamurjonovmamurjon01@gmail.com>
2024-07-25 12:02:25 +0500

    done

---
+++ index.js
@@ -0,0 +1,33 @@
+let products = document.querySelector(".products");
+
+fetch("https://fakestoreapi.com/products?limit=20")
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+    for (let product of data) {
+        let card = document.createElement("div");
+        let image = document.createElement("img");
+        let title = document.createElement("h1");
+        let price = document.createElement("li");
+        let rating = document.createElement("li");
+        let list = document.createElement("ul");
+
+        card.classList.add("card");
+
+        title.textContent = product.title;
+        price.textContent = product.price + "$";
+        rating.textContent = product.rating.rate;
+        image.src = product.image;
+        image.alt = product.title;
+
+        list.appendChild(price);
+        list.appendChild(rating);
+
+        card.appendChild(image);
+        card.appendChild(title);
+        card.appendChild(list);
+
+        products.appendChild(card);
+    };
+});
